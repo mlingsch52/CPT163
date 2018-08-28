8b57d391ea2f33bc4ff298b22158ef710e67a962
Michael Lingsch <mlingsch@skyword.com>
2018-08-28 13:39:56 -0400

    Hello World

---
+++ src/MyFirstProgram.java
@@ -0,0 +1,16 @@
+/*********************************
+CPT163
+Micahel Lingsch
+My First Java Program
+This program will print "Hello World"
+Created On: 08/24/2018
+Last Modified: 08/24/2018
+ *********************************/
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
