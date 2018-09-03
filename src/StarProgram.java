b9f45e4b28c0b32173cc6a26abe7d8396877fadc
Michael Lingsch <mlingsch@skyword.com>
2018-09-03 14:52:00 -0400

    Code cleanup for star program

--- src/StarProgram.java
+++ src/StarProgram.java
@@ -11,13 +11,13 @@
 public class StarProgram {
 
     public static void main(String[] args) {
-        System.out.println("   *   ");
-        System.out.println("  ***  ");
-        System.out.println(" ***** ");
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
         System.out.println("*******");
-        System.out.println(" ***** ");
-        System.out.println("  ***  ");
-        System.out.println("   *   ");
+        System.out.println(" *****");
+        System.out.println("  ***");
+        System.out.println("   *");
     }
 }
 
