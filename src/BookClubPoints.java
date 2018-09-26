9d423f0018a2b73e6fb7af7094a2a5ebc1a9790a
Michael Lingsch <mlingsch@skyword.com>
2018-09-26 08:08:24 -0400

    book club updates

--- src/BookClubPoints.java
+++ src/BookClubPoints.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.text.NumberFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 /*********************************
  CPT163
@@ -12,11 +15,54 @@ import javax.swing.*;
 
 public class BookClubPoints {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        //Define our variables
+        String[] currentMonthName = {"January", "February", "March", "April", "May", "June", "July",
+                "August", "September", "October", "November", "December"};
+        String booksPurchasedPerMonth;
+        int booksPurchased, pointsEarned;
 
+        //Establish the Current Month
+        Calendar cal = Calendar.getInstance();
+        String currentMonth = currentMonthName[cal.get(Calendar.MONTH)];
+
+        //Establish how many books were purchased in the Current Month
+        booksPurchasedPerMonth = JOptionPane.showInputDialog("How many books have you purchased so far for the month of " + currentMonth + "?");
+        booksPurchased = Integer.parseInt(booksPurchasedPerMonth);
+
+        //Calculate how many points they've earned this month
+        if (booksPurchased == 0) {
+            JOptionPane.showMessageDialog(null, "You've purchased 0 books in the month of " + currentMonth + ".\n" +
+                    "You have earned 0 points. I'm disappointed in you.");
+        } else if (booksPurchased == 1) {
+            pointsEarned = 5;
+            JOptionPane.showMessageDialog(null, "You've purchased 1 book in the month of " + currentMonth + ".\n" +
+                    "You have earned " + pointsEarned + " points in " + currentMonth + ".");
+        } else if (booksPurchased == 2) {
+            pointsEarned = 15;
+            JOptionPane.showMessageDialog(null, "You've purchased 2 books in the month of " + currentMonth + ".\n" +
+                    "You have earned " + pointsEarned + " points in " + currentMonth + ".");
+
+        } else if (booksPurchased == 3) {
+            pointsEarned = 30;
+            JOptionPane.showMessageDialog(null, "You've purchased 3 books in the month of " + currentMonth + ".\n" +
+                    "You have earned " + pointsEarned + " points in " + currentMonth + ".");
+
+        } else if (booksPurchased == 4) {
+            pointsEarned = 60;
+            JOptionPane.showMessageDialog(null, "You've purchased 4 books in the month of " + currentMonth + ".\n" +
+                    "You have earned " + pointsEarned + " points in " + currentMonth + ".");
+
+        } else if (booksPurchased > 4) {
+            pointsEarned = 60;
+            JOptionPane.showMessageDialog(null, "You've purchased " + booksPurchased + " books in the month of " + currentMonth + ".\n" +
+                    "You have earned " + pointsEarned + " points in " + currentMonth + ".");
+
+        } else if (booksPurchased < 0) {
+            JOptionPane.showMessageDialog(null, "You haven't purchased any books! Get your act together. Zero points!");
+
+        }
 
     }
-
-
 }
